7852d1c068c032d5e0614abc9d22cd0975ee1a31
Developer <developer@example.com>
2026-05-23 11:29:25 -0700

    test: add config unit tests and convert test-actual to ESM

--- test-actual.js
+++ test-actual.js
@@ -1,4 +1,4 @@
-const api = require('@actual-app/api');
+import api from '@actual-app/api';
 async function run() {
   try {
     await api.init({
@@ -8,6 +8,7 @@ async function run() {
     });
     const files = await api.getRemoteFiles();
     console.log(JSON.stringify(files, null, 2));
+    await api.shutdown();
   } catch (e) {
     console.error(e.message);
     process.exit(1);
